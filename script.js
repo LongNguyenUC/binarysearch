683bb58fd0db2f6ee968a5acfd499c11363b5479
LongNguyenUC <192123324+LongNguyenUC@users.noreply.github.com>
2025-02-10 22:05:37 -0500

    Increase amount of boxes to 16 and made traverse animation stop at the user input

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
 const search = document.querySelector("#Search");
 const numberinput = document.querySelector("#number-input");
+const message = document.querySelector(".message-container");
+
+console.log("hello")
 
 search.addEventListener("click", traverse);
 
 let boxes = ["tmp","one", "two","three","four","five","six","seven", "eight"];
 
 function traverse(){
-    for(let i = 1; i <= 8; i++){
+    //Below is a possible solution for clearing timers
+    let intendedColor = "green";
+
+
+    let find = parseFloat(numberinput.value);
+    let psuedobool = 0;
+
+    const blocks = document.getElementsByClassName("box");
+    console.log(find);
+    for(let i = 1; i <= 16; i++){
         // setTimeout(blockName(boxes[i]), 1000); Old Funtion
 
         setTimeout(function() {
-            current(boxes[i], "green");
+            // current(i - 1, "green");
+            blocks[i - 1].style.backgroundColor = intendedColor;
+            if(blocks[i-1].innerText == find){
+                console.log("works!");
+                message.innerText="Found!"
+                psuedobool = 1;
+                intendedColor = "#a8abc7";
+                setTimeout(function(){
+                    message.innerText ="";
+                }, 100 *i);
+            }
         }, 1000 * i);
 
+
         setTimeout(function() {
-            current(boxes[i], "#a8abc7");
+            // current(i - 1, "#a8abc7");
+            blocks[i - 1].style.backgroundColor = "#a8abc7";
         }, 1000 * (i + 1));
         // document.getElementsByClassName(boxes[i]).style.backgroundColor = "#a8abc7";
     }
 }
 
-function current(blockName, color){
-    document.getElementsByClassName(blockName)[0].style.backgroundColor = color;
-}
+// function current(blockName, color){
+//     .style.backgroundColor = color;
+// }
